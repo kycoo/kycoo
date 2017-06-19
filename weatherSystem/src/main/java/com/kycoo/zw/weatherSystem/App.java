df98940f6a0e11f5aaa2d5255baca2cc532296b1
heecoo <1107455107@qq.com>
2017-06-19 15:04:36 +0800

    修改
    输出hekai

--- weatherSystem/src/main/java/com/kycoo/zw/weatherSystem/App.java
+++ weatherSystem/src/main/java/com/kycoo/zw/weatherSystem/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+		System.out.println("heikai");
     	System.out.println("zhangwang");
     	System.out.print("132457689");
         System.out.println( "Hello World!" );

988c13c0d42ec351067fe259bd3a1f1882b22633
zwanggogo <892499846@qq.com>
2017-06-19 16:59:26 +0800

    重新创建项目
    删除之前的文件，重新创建项目

---
+++ weatherSystem/src/main/java/com/kycoo/domain/City.java
@@ -0,0 +1,11 @@
+package com.kycoo.domain;
+
+/**
+ * 
+ * @author 张汪
+ *
+ *	城市类，描述城市的基本信息
+ */
+public class City {
+
+}
---
+++ weatherSystem/src/main/java/com/kycoo/domain/Weather.java
@@ -0,0 +1,10 @@
+package com.kycoo.domain;
+
+/**
+ * 
+ * @author 张汪
+ *	描述天气实体
+ */
+public class Weather {
+	
+}

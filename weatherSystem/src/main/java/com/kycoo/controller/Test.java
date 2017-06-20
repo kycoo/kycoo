0d58790c044b43a8ab168ad4a614e513dd3a75cf
zwanggogo <892499846@qq.com>
2017-06-20 17:20:01 +0800

    add DayWeatherClass

--- weatherSystem/src/main/java/com/kycoo/controller/Test.java
+++ weatherSystem/src/main/java/com/kycoo/controller/Test.java
@@ -4,13 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kycoo.utils.WeatherUtil;
+
 @Controller
 public class Test {
 
 	@GetMapping(name="/test")
 	@ResponseBody
 	public String test(){
-		
+		WeatherUtil.getWeatherByIp("182.149.163.34");
 		return "hellosdas";
 	}
 }
---
+++ weatherSystem/src/main/java/com/kycoo/utils/WeatherUtil.java
@@ -0,0 +1,69 @@
+package com.kycoo.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.kycoo.domain.Weather;
+
+public final class WeatherUtil {
+	
+	private WeatherUtil() {
+		
+		throw new AssertionError();
+	}
+	
+	private static final String HOST="http://saweather.market.alicloudapi.com";
+	
+	private static final String APP_CODE="6b8c74b1ee014586b0dda28a56d487b9";
+	
+//	private static final String 
+	
+	public static Weather getWeatherByIp(String ip){
+	    String path = "/ip-to-weather";
+	    String method = "GET";
+	    Map<String, String> headers = new HashMap<String, String>();
+	    //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+	    headers.put("Authorization", "APPCODE " + APP_CODE);
+	    Map<String, String> querys = new HashMap<String, String>();
+	    querys.put("ip", ip);
+	    querys.put("need3HourForcast", "0");
+	    querys.put("needAlarm", "0");
+	    querys.put("needHourData", "0");
+	    querys.put("needIndex", "0");
+	    querys.put("needMoreDay", "0");
+	    try {
+	    	/**
+	    	* 重要提示如下:
+	    	* HttpUtils请从
+	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+	    	* 下载
+	    	*
+	    	* 相应的依赖请参照
+	    	* https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+	    	*/
+	    	HttpResponse response = HttpUtils.doGet(HOST, path, method, headers, querys);
+	    	System.out.println(response.toString());
+	    	//获取response的body
+	    	String jsonString = EntityUtils.toString(response.getEntity());
+	    	JSONObject  jsonObject = JSONObject.parseObject(jsonString);
+	    	jsonObject = jsonObject.getJSONObject("showapi_res_body");
+	    	jsonObject.getJSONObject("now");
+	    	System.out.println(jsonObject.getString("f1").toString());
+//	    	JSONObject.parseObject(jsonObject.getString("f1").toString(), WeatherDay.class);
+//	    	JSONObject.p
+	    } catch (Exception e) {
+	    	e.printStackTrace();
+	    }
+		return null;
+		
+	}
+	
+	
+}
